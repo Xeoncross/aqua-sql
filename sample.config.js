f8936481dada1dc154932f5c7c4dbd79821ad333
xeoncross <david@xeoncross.com>
2017-10-30 13:35:35 -0500

    Updated eslintrc. Moved config out of source control

--- sample.config.js
+++ sample.config.js
@@ -2,16 +2,14 @@
 const Confidence = require('confidence');
 const Dotenv = require('dotenv');
 
-
 Dotenv.config({ silent: true });
 
 const criteria = {
     env: process.env.NODE_ENV
 };
 
-
 const config = {
-    $meta: 'This file configures the MacDaddy device.',
+    $meta: 'This file configures the Aqua device.',
     projectName: 'Aqua',
     port: {
         web: {
@@ -33,55 +31,50 @@ const config = {
     },
     cookieSecret: {
         $filter: 'env',
-        production: process.env.COOKIE_SECRET,
+        production: '',
         $default: '!k3yb04rdK4tz~4qu4~k3yb04rdd0gz!'
     },
-    db : {
+    db: {
+        dialect: 'postgres',
         host: 'localhost',
         database: 'aqua',
         username: 'aqua',
-        password: process.env.DB_PASSWORD
-
+        password: ''
     },
-    db_test : {
+    db_test: {
+        dialect: 'postgres',
         host: 'localhost',
         database: 'aqua_test',
         username: 'aqua_test',
         password: 'test'
-
     },
     nodemailer: {
         host: 'smtp.gmail.com',
         port: 465,
         secure: true,
         auth: {
-            user: 'jimlowrey@gmail.com',
-            pass: process.env.SMTP_PASSWORD
+            user: 'gmail@gmail.com',
+            pass: ''
         }
     },
     system: {
         fromAddress: {
             name: 'E-Aqua',
-            address: 'jimlowrey@gmail.com'
+            address: 'gmail@gmail.com'
         },
         toAddress: {
             name: 'E-Aqua',
-            address: 'jimlowrey@gmail.com'
+            address: 'gmail@gmail.com'
         }
     }
 };
 
-
 const store = new Confidence.Store(config);
 
-
 exports.get = function (key) {
-
     return store.get(key, criteria);
 };
 
-
 exports.meta = function (key) {
-
     return store.meta(key, criteria);
 };
